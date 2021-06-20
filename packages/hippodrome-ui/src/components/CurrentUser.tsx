d5145c347616589ccbeb1db5492766730a082537
Topper Bowers <topper@toppingdesign.com>
2021-06-20 21:16:27 +0200

    add jazzicon

--- packages/hippodrome-ui/src/components/CurrentUser.tsx
+++ packages/hippodrome-ui/src/components/CurrentUser.tsx
@@ -1,13 +1,20 @@
+import { Text, Box, HStack } from '@chakra-ui/react'
 import React from 'react'
 import { useChainContext } from '../hooks/useChainContext'
 import ConnectButton from './ConnectButton'
+import { Jazzicon } from '@ukstv/jazzicon-react'
 
 const CurrentUser:React.FC = () => {
   const { connected, address } = useChainContext()
 
   if (connected) {
     return (
-      <p>{address}</p>
+      <HStack alignItems="center">
+        <Box h={12} w={12}>
+          <Jazzicon address={address!} />
+        </Box>
+        <Text>{address}</Text>
+      </HStack>
     )
   }
 
