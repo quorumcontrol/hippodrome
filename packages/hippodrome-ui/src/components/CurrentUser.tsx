eb31f55f35bf94de9dec97d9ff00c0105eabbb49
alabobriggs <alabobriggs@gmail.com>
2021-07-02 16:00:56 +0100

    add copied address

--- packages/hippodrome-ui/src/components/CurrentUser.tsx
+++ packages/hippodrome-ui/src/components/CurrentUser.tsx
@@ -1,27 +1,42 @@
-import { Text, Box, HStack } from '@chakra-ui/react'
-import React from 'react'
-import { useChainContext } from '../hooks/useChainContext'
-import ConnectButton from './ConnectButton'
-import { Jazzicon } from '@ukstv/jazzicon-react'
+import { Text, Box, HStack, useToast } from "@chakra-ui/react"
+import React from "react"
+import { useChainContext } from "../hooks/useChainContext"
+import ConnectButton from "./ConnectButton"
+import { Jazzicon } from "@ukstv/jazzicon-react"
+import { centeredTruncateText } from "../utils/truncateText"
 
-const CurrentUser:React.FC = () => {
+const CurrentUser: React.FC = () => {
   const { connected, address } = useChainContext()
+  const toast = useToast()
+
+  const ClipURLcopyHandler = () => {
+    navigator.clipboard.writeText(address || "")
+
+    toast({
+      title: "Address copied",
+      duration: 4000,
+      status: "success",
+      position: "bottom-right",
+    })
+  }
 
   if (connected) {
     return (
-      <HStack alignItems="center">
+      <HStack
+        alignItems="center"
+        cursor="copy"
+        role="button"
+        onClick={ClipURLcopyHandler}
+      >
         <Box h={12} w={12}>
           <Jazzicon address={address!} />
         </Box>
-        <Text>{address}</Text>
+        <Text fontWeight="semibold">{centeredTruncateText(address!, 5)}</Text>
       </HStack>
     )
   }
 
-  return (
-    <ConnectButton />
-  )
-
+  return <ConnectButton />
 }
 
 export default CurrentUser
