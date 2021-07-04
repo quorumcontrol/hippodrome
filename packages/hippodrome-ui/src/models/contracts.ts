16e9470f6ef74c6b976b987d9db0e0634f919750
Topper Bowers <topper@toppingdesign.com>
2021-07-04 20:57:39 +0200

    in theory this works

--- packages/hippodrome-ui/src/models/contracts.ts
+++ packages/hippodrome-ui/src/models/contracts.ts
@@ -7,7 +7,7 @@ import { BalanceShifter } from '../types/ethers-contracts/BalanceShifter'
 export const MINTER_ADDRESS = '0xD7bb140b53EB814aa10f051B76B0e5b2458fBcAd'
 export const BALANCE_SHIFTER_ADDRESS = '0x4cEAD3Fe4994f6a01d1c24E8C96843c6Dc7ceD5d'
 
-const SUSHI_ROUTER_ADDRESS = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506'
+export const SUSHI_ROUTER_ADDRESS = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506'
 
 export const minter = () => {
   if (!chainInstance.relayer || !chainInstance.signer) {
