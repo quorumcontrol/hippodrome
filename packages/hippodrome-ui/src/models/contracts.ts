c5e463a518e9e1e342556246ae813786b39eb7bf
Topper Bowers <topper@toppingdesign.com>
2021-07-17 16:59:48 +0200

    Revert "fixes"
    This reverts commit e3a378e4861e77e88c0e6ff5e7e18e78df06437d.

--- packages/hippodrome-ui/src/models/contracts.ts
+++ packages/hippodrome-ui/src/models/contracts.ts
@@ -1,17 +1,17 @@
 import { IChain } from './chain'
 import { wrapContract } from 'kasumah-relay-wrapper/dist/src'
-import { UnderwriteableMinter, UnderwriteableMinter__factory, UniswapV2Router02__factory } from '../types/ethers-contracts'
+import { UnderwriteableMinter, UnderwriteableMinter__factory, UniswapV2Router02__factory, UniswapV2Router02 } from '../types/ethers-contracts'
 import { BalanceShifter__factory } from '../types/ethers-contracts/factories/BalanceShifter__factory'
 import { BalanceShifter } from '../types/ethers-contracts/BalanceShifter'
 
-export const MINTER_ADDRESS = '0xD7bb140b53EB814aa10f051B76B0e5b2458fBcAd'.toLowerCase()
-export const BALANCE_SHIFTER_ADDRESS = '0x4cEAD3Fe4994f6a01d1c24E8C96843c6Dc7ceD5d'.toLowerCase()
+export const MINTER_ADDRESS = '0xD7bb140b53EB814aa10f051B76B0e5b2458fBcAd'
+export const BALANCE_SHIFTER_ADDRESS = '0x4cEAD3Fe4994f6a01d1c24E8C96843c6Dc7ceD5d'
 
-export const SUSHI_ROUTER_ADDRESS = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506'.toLowerCase()
-export const COMETH_ROUTER_ADDRESS = "0x93bcDc45f7e62f89a8e901DC4A0E2c6C427D9F25".toLowerCase()
+export const SUSHI_ROUTER_ADDRESS = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506'
+export const COMETH_ROUTER_ADDRESS = "0x93bcDc45f7e62f89a8e901DC4A0E2c6C427D9F25"
 
-export const WPTG_ADDRESS = "0xc0f14c88250e680ecd70224b7fba82b7c6560d12".toLowerCase()
-export const RENDOGE_ADDRESS = "0xcE829A89d4A55a63418bcC43F00145adef0eDB8E".toLowerCase()
+export const WPTG_ADDRESS = "0xc0f14c88250e680ecd70224b7fba82b7c6560d12"
+export const RENDOGE_ADDRESS = "0xcE829A89d4A55a63418bcC43F00145adef0eDB8E"
 
 export const minter = (chain:IChain) => {
   if (!chain.relayer || !chain.signer) {
