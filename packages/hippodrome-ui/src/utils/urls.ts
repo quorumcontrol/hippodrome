3f23d0c4c43582ac49379a32a842ae3eeb63b621
Topper Bowers <topper@toppingdesign.com>
2021-10-03 19:37:36 +0200

    bad url was showig up as forwardTo=undefined

--- packages/hippodrome-ui/src/utils/urls.ts
+++ packages/hippodrome-ui/src/utils/urls.ts
@@ -9,7 +9,8 @@ export interface MintUrlParams {
 
 export const mintUrl = ({ inputToken, to, nonce, swap, pool, forwardTo }: MintUrlParams) => {
   if (swap) {
-    return `/transaction/mint/${inputToken}/${to}/${nonce}?swap=${swap}&forwardTo=${forwardTo}`;
+    const forwardToStr = forwardTo ? `&forwardTo=${forwardTo}` : ''
+    return `/transaction/mint/${inputToken}/${to}/${nonce}?swap=${swap}${forwardToStr}`;
   }
   return `/transaction/mint/${inputToken}/${to}/${nonce}?pool=${pool}`;
 };
